4bf71945bfa42730f7cb51ebcbc334dc09e1d2b4
Kaiser, Tim <Tim.Kaiser@adidas.com>
2019-04-03 12:40:57 +0200

    added TODO file; stored books-data in src/data folder; added and started configuration of tslint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import express from 'express' 
+// const express = require("express")
+import * as express from "express";
+// import data from './data/books.js'
 
 const app = express();
+
+app.get("/books", (req, res) => {
+    res.send("ciao");
+});
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on localhost:${PORT} ...`);
+    return `Server is listening on localhost:${PORT} ...`;
 });
