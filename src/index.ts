fb44d65ce3030e972964db41e389e1e02968b298
Kaiser, Tim <Tim.Kaiser@adidas.com>
2019-04-03 15:12:20 +0200

    modified TODO; added book/:id route to routes folder and to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as express from "express";
-import * as booksRouter from "./routes/books";
+
+import booksRouter from "./routes/books";
+import booksIDRouter from "./routes/booksID";
 
 const app = express();
 
 const PORT = 3000;
 
 app.use("/books", booksRouter);
+app.use("/books", booksIDRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on localhost:${PORT} ...`);
