d23e6d3c99c0a6a6929fa53c4820fa3fbd4f61fc
Kaiser, Tim <Tim.Kaiser@adidas.com>
2019-04-04 09:44:44 +0200

    added book/:id route to books route; modified TODO and index.ts

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -7,4 +7,13 @@ router.get("/", (req, res) => {
   res.send(data);
 });
 
+router.get("/:id", (req, res) => {
+  if (req.params.id > data.length || req.params.id < 1) {
+    res.send("This book does not exist ...");
+  } else {
+    const index = req.params.id - 1;
+    res.send(data[index]);
+  }
+});
+
 export default router;
